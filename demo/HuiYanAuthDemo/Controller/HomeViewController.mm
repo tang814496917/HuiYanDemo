bb05297df2af59dbfb68cfbf8e556f4d828ef57d
tang814496917 <814496917@qq.com>
2023-07-24 10:05:02 +0800

    add

--- demo/HuiYanAuthDemo/Controller/HomeViewController.mm
+++ demo/HuiYanAuthDemo/Controller/HomeViewController.mm
@@ -80,6 +80,62 @@
 - (void)push:(UIButton *)button{
     switch (button.tag) {
         case 100:
+        {
+            
+            //            //重试次数  默认5次
+            //            @property (nonatomic, assign) NSInteger restartCount;
+            //
+            //            //准备阶段 永不超时   默认NO
+            //            @property (nonatomic, assign) BOOL isNeverTimeOut;
+            //
+            //            //准备阶段 超时时间  毫秒   默认 15000
+            //            @property (nonatomic, assign) NSInteger prepareTimeOut;
+            //
+            //            //控制一闪交互流程; 0-动作+反光；1-反光；2-动作.
+            //            @property (nonatomic, assign) NSInteger actref_ux_mode;
+            //
+            //            // 动作阶段 超时时间  毫秒   默认 15000
+            //            @property (nonatomic, assign) NSInteger actionTimeoutMs;
+            //
+            //            // 是否使用默认动作    默认YES
+            //            @property (nonatomic, assign) BOOL isDefaultAction;
+            //
+            //            //  指定需要下发的动作; 1 blink，2 mouth，3 node，4 shake，5 silence; 若需要连续多个动作则使用逗号隔开.
+            //            //默认 @[@1,@2,@3,@4];
+            //            @property (nonatomic, strong) NSArray * action_data;
+            //
+            //            //是否使用随机动作 默认NO
+            //            @property (nonatomic, assign) BOOL action_random;
+            //
+            //            //是否静音 默认NO
+            //            @property (nonatomic, assign) BOOL mute;
+            //
+            //            //是否最佳人脸压缩  默认YES
+            //            @property (nonatomic, assign) BOOL isUseBestFaceImage;
+            //
+            //            // 接口地址
+            //            @property (nonatomic, strong) NSString * hostUrl;
+            //
+            //            // License文件路径
+            //            @property (strong, nonatomic) NSString *authLicense;
+            //
+            //            // 图灵顿授权文件路径
+            //            @property (nonatomic, strong) NSString *riskLicense;
+            
+            HYConfigModel *model = [HYConfigModel new];
+            model.actref_ux_mode = 0;
+            model.action_data = @[@1,@2,@3,@4];
+            model.action_random = NO;
+            model.mute = NO;
+            model.isUseBestFaceImage = YES;
+            model.actionTimeoutMs = 15000;
+            model.prepareTimeOut = 15000;
+            model.restartCount = 5;
+            model.isNeverTimeOut = NO;
+            model.hostUrl = @"https://biology-port.yz-intelligence.com:9978/";
+            model.isDefaultAction = YES;
+            model.authLicense = [[NSBundle mainBundle] pathForResource:@"licsence.lic" ofType:@""];
+            model.riskLicense = [[NSBundle mainBundle] pathForResource:@"sdcs_test_androids.lic" ofType:@""];
             [HYAuthApi startAuth:[HYConfigModel new] withSuccCallback:^(NSDictionary * _Nonnull reportData) {
                 NSData *jsonData = [NSJSONSerialization dataWithJSONObject:reportData
                                                                    options:0
@@ -88,13 +144,16 @@
                                                              encoding:NSUTF8StringEncoding];
                 NSLog(@"report:%@",jsonString);
                 [self.navigationController pushViewController:[ParamSettingViewController new] animated:YES];
-
+                
             } withFailCallback:^(int errCode, NSString * _Nonnull errMsg) {
                 [self.navigationController pushViewController:[ParamSettingViewController new] animated:YES];
             }];
+        }
             break;
         case 101:
+        {
             [self.navigationController pushViewController:[ParamSettingViewController new] animated:YES];
+        }
             break;
         case 102:
     
